83e36321268eed2dc1d7f0d93239739102c7baf5
Krzysztof Leśniewski <krzysztof.lesniewski.wroc@gmail.com>
2019-02-05 11:02:23 +0100

    final version of logic. UI is working

--- js/main.js
+++ js/main.js
@@ -6,12 +6,19 @@ function GameOfLife(boardWidth, boardHeight, board) {
     this.boardLong = 0;
     this.cells = [];
     this.nexGenCells = [];
+    this.nextGenCounter = 0;
+    this.sizeX = this.height / this.boardItemSize;
+    this.sizeY = this.width / this.boardItemSize;
 
     this.createBoard = function () {
+        if (this.board.hasChildNodes()) {
+            while (this.board.hasChildNodes()) {
+                this.board.removeChild(this.board.firstElementChild);
+            }
+        }
         this.board.style.width = `${this.width}px`;
         this.board.style.height = `${this.height}px`;
-        this.boardLong = (this.width / this.boardItemSize * this.height / this.boardItemSize);
-
+        this.boardLong = (this.sizeY * this.sizeX);
         for (let i = 0; i < this.boardLong; i++) {
             let data = document.createElement("div");
             data.addEventListener("click", this.changeStateHandler);
@@ -20,6 +27,7 @@ function GameOfLife(boardWidth, boardHeight, board) {
             this.board.append(data);
             this.cells.push(data);
         }
+
     };
     // event listener callback
     this.changeStateHandler = function () {
@@ -49,9 +57,8 @@ function GameOfLife(boardWidth, boardHeight, board) {
         this.setCellState(22, 20, true);
         this.setCellState(20, 21, true);
         this.setCellState(21, 22, true);
-
-
     };
+
     // calculation of cell state in nex gen
     this.computeCellNextState = function (x, y) {
         let siblings = 0;
@@ -80,10 +87,8 @@ function GameOfLife(boardWidth, boardHeight, board) {
     };
 
     this.computeNextGeneration = function () {
-        let sizeX = this.height / this.boardItemSize;
-        let sizeY = this.width / this.boardItemSize;
-        for (let i = 0; i < sizeY; i++) {
-            for (let j = 0; j < sizeX; j++) {
+        for (let i = 0; i < this.sizeY; i++) {
+            for (let j = 0; j < this.sizeX; j++) {
                 if (this.computeCellNextState(i, j)) {
                     this.setCellState(i, j, 1);
                 } else {
@@ -94,34 +99,87 @@ function GameOfLife(boardWidth, boardHeight, board) {
     };
 
     this.printNextGeneration = function () {
-        for (let i = 0; i < 1199; i++) {
+        this.nextGenCounter++;
+        for (let i = 0; i < this.sizeY * this.sizeX - 1; i++) {
             this.cells[i].classList.remove("live");
             if (this.nexGenCells[i] === 1) {
                 this.cells[i].classList.add("live");
             }
         }
     };
+
+    this.clearBoard = function () {
+        this.nexGenCells = Array().fill(0);
+        this.printNextGeneration();
+        this.nextGenCounter = 0;
+    };
+
+    this.start = function () {
+        this.nextGenCounter = 0;
+        this.createBoard();
+        this.firstGlider();
+    };
 }
 
+function genCounterCalc(data, value) {
+    data.innerText = value;
+}
 
 document.addEventListener("DOMContentLoaded", function () {
 
-    const sizeX = document.getElementById("sizeX");
-    const sizeY = document.getElementById("sizeY");
+    let sizeX = document.getElementById("sizeX");
+    let sizeY = document.getElementById("sizeY");
     sizeX.value = 800;
     sizeY.value = 600;
     const board = document.getElementById("board");
-
+    const genCounter = document.getElementById("genCounter");
+    const playBtn = document.getElementById("play");
+    const pauseBtn = document.getElementById("pause");
+    const setSize = document.getElementById("setSize");
+    const clearBoard = document.getElementById("clearBoard");
 
     let game = new GameOfLife(sizeX.value, sizeY.value, board);
-    game.createBoard();
-    game.firstGlider();
+    game.start();
+
+    var start;
+    var isAnimationON = false;
+
+    playBtn.addEventListener("click", () => {
+        if (isAnimationON === false) {
+            start = setInterval(() => {
+                game.computeNextGeneration();
+                game.printNextGeneration();
+                console.log(genCounter.innerText);
+                genCounterCalc(genCounter, game.nextGenCounter);
+            }, 500);
+            isAnimationON = true;
+        }
+    });
 
 
-    setInterval(() => {
-        game.computeNextGeneration();
-        game.printNextGeneration();
-    }, 500);
+    pauseBtn.addEventListener("click", () => {
+        clearTimeout(start);
+        isAnimationON = false;
+    });
+
+
+    clearBoard.addEventListener("click", () => {
+        // clearTimeout(start);
+        isAnimationON = false;
+        clearTimeout(start);
+        game.clearBoard();
+        genCounter.innerText =0;
+    });
+
+
+    setSize.addEventListener("click", () => {
+        sizeX = document.getElementById("sizeX");
+        sizeY = document.getElementById("sizeY");
+        clearTimeout(start);
+        game = new GameOfLife(sizeX.value, sizeY.value, board);
+        game.start();
+        genCounterCalc(genCounter, game.nextGenCounter);
+    })
 
 
 });
